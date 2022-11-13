79c4136112e9a9bc6cb1b064aaaf5edb759ea36f
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-13 15:51:35 +0900

    feat: 플레이어의 구입 금액이 올바른지 테스트하라

---
+++ src/test/java/lotto/view/PlayerInputViewTest.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import lotto.model.Player;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerInputViewTest {
+
+    @Test
+    @DisplayName("구입금액이 1000원 단위이어야 한다.")
+    void createInputMoneyTest1() {
+        String money = "0";
+
+        assertThatThrownBy(() -> PlayerInputView.validateInputMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("구입금액 모두 숫자이어야 한다.")
+    void createInputMoneyTest2() {
+        String money = "10 00";
+
+        assertThatThrownBy(() -> PlayerInputView.validateInputMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
