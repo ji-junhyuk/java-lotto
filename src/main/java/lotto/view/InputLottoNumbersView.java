39b5feb3a0969ec7e0291b18f1cd2bb573bc3721
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-13 23:05:22 +0900

    feat: 유효한 보너스 번호를 받았는지 테스트하라

--- src/main/java/lotto/view/InputLottoNumbersView.java
+++ src/main/java/lotto/view/InputLottoNumbersView.java
@@ -11,10 +11,15 @@ import java.util.stream.Collectors;
 public class InputLottoNumbersView {
 
     private static final String POSSIBLE_LOTTO_WINNING_NUMBERS = "[0-9,]+";
+    private static final String POSSIBLE_BONUS_NUMBER = "[0-9]+";
     private static final String ASK_LOTTO_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String OUT_OF_RANGE_ERROR_MESSAGE = "[ERROR] 로또 당첨번호는 1~45사이의 숫자이어야 합니다.";
-    private static final String DUPLICATE_AND_NOT_SIX_ERROR_MESSAGE = "[ERROR] 로또 당첨번호는 중복없이 6개의 숫자이어야 합니다.";
+    private static final String DUPLICATE_AND_NOT_MATCH_SIZE_ERROR_MESSAGE = "[ERROR] 로또 당첨번호는 중복없는 숫자이어야 합니다.";
     private static final String ONLY_NUMBER_COMMA_ERROR_MESSAGE = "[ERROR] 로또 당첨번호는 숫자와 콤마로 이루어져야 합니다.";
+
+    private static final String ONLY_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 보너스번호는 숫자로 이루어져야 합니다.";
+    private static final String ASK_LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public static void GetWinningNumbers() {
         String line;
 
@@ -24,29 +29,49 @@ public class InputLottoNumbersView {
         List<Integer> lottoWinningNumbers = Arrays.stream(line.split(","))
                 .map(element-> Integer.parseInt(element))
                 .collect(Collectors.toList());
-        validateLottoWinningNumbers(lottoWinningNumbers);
+        validateLottoWinningNumbers(lottoWinningNumbers, false);
+        GetBonusNumber(lottoWinningNumbers);
     }
 
-    public static void validateLottoWinningNumbers(List<Integer> lottoWinningNumbers) {
-        validateWinningNumberSize(lottoWinningNumbers);
-        validateWinningNumberRange(lottoWinningNumbers);
+    private static void GetBonusNumber(List<Integer> lottoWinningNumbers) {
+        String line;
+
+        System.out.println(ASK_LOTTO_BONUS_NUMBER_MESSAGE);
+        line = Console.readLine();
+        validateOnlyNumber(line);
+        lottoWinningNumbers.add(Integer.parseInt(line));
+        validateLottoWinningNumbers(lottoWinningNumbers, true);
     }
 
-    private static void validateWinningNumberRange(List<Integer> lottoWinningNumbers) {
-        for (int idx = 0; idx < 6; ++idx) {
+    public static void validateOnlyNumber(String line) {
+        if (!line.matches(POSSIBLE_BONUS_NUMBER))
+            throw new IllegalArgumentException(ONLY_NUMBER_ERROR_MESSAGE);
+    }
+
+    public static void validateLottoWinningNumbers(List<Integer> lottoWinningNumbers, boolean bonus_number) {
+        int size = 6;
+
+        if (bonus_number == true)
+            size += 1;
+        validateWinningNumberSize(lottoWinningNumbers, size);
+        validateWinningNumberRange(lottoWinningNumbers, size);
+    }
+
+    private static void validateWinningNumberRange(List<Integer> lottoWinningNumbers, int size) {
+        for (int idx = 0; idx < size; ++idx) {
             if (lottoWinningNumbers.get(idx) < 1 || lottoWinningNumbers.get(idx) > 45) {
                 throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
             }
         }
     }
 
-    private static void validateWinningNumberSize(List<Integer> lottoWinningNumbers) {
+    private static void validateWinningNumberSize(List<Integer> lottoWinningNumbers, int size) {
         Set<Integer> integerSet = new HashSet<>();
         for (int idx = 0; idx < lottoWinningNumbers.size(); ++idx) {
             integerSet.add(lottoWinningNumbers.get(idx));
         }
-        if (integerSet.size() != 6) {
-            throw new IllegalArgumentException(DUPLICATE_AND_NOT_SIX_ERROR_MESSAGE);
+        if (integerSet.size() != size) {
+            throw new IllegalArgumentException(DUPLICATE_AND_NOT_MATCH_SIZE_ERROR_MESSAGE);
         }
     }
 
--- src/test/java/lotto/view/InputLottoNumbersViewTest.java
+++ src/test/java/lotto/view/InputLottoNumbersViewTest.java
@@ -28,18 +28,29 @@ public class InputLottoNumbersViewTest {
     }
 
     @Test
-    @DisplayName("로또 당첨번호는 중복 없이 6개 숫자이어야 한다.(case: 7개)")
+    @DisplayName("로또 보너스 번호는 숫자로만 이루어져야 한다.(case: @)")
     void InputWinningNumbersTest3() {
-
-        assertThatThrownBy(() -> InputLottoNumbersView.validateLottoWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> InputLottoNumbersView.validateOnlyNumber("1@"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    @DisplayName("로또 당첨번호는 중복 없이 6개 숫자이어야 한다.(case: 중복숫자)")
+    @DisplayName("로또 당첨번호는 중복 없이 6개의 숫자이어야 한다.(case: 7개)")
     void InputWinningNumbersTest4() {
+        assertThatThrownBy(() -> InputLottoNumbersView.validateLottoWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), false))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("로또 당첨번호(보너스포함)는 중복 없이 7개의 숫자이어야 한다.(case: 중복숫자)")
+    void InputWinningNumbersTest5() {
+        assertThatThrownBy(() -> InputLottoNumbersView.validateLottoWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 6), true))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        assertThatThrownBy(() -> InputLottoNumbersView.validateLottoWinningNumbers(List.of(1, 2, 3, 4, 5, 5)))
+    @Test
+    @DisplayName("로또 당첨번호(보너스포함)는 중복 없이 7개의 숫자이어야 한다.(case: 8개)")
+    void InputWinningNumbersTest6() {
+        assertThatThrownBy(() -> InputLottoNumbersView.validateLottoWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7, 8), true))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
